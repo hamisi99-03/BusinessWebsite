64f320c0f14f6aa4741eb80be27f85ed76e08e3e
Hamisi <yuzalsif2@gmai.com>
2026-09-23 21:35:32 +0300

    fix: prevent checkout 500 from hanging SMTP notifications

--- ecommerce_manager/settings.py
+++ ecommerce_manager/settings.py
@@ -221,6 +221,7 @@ EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
+EMAIL_TIMEOUT = int(os.environ.get('EMAIL_TIMEOUT', 10))
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # Media files for production
